c59f353ba0016458c3e69f939d6897bc87d170b6
DPLIT\Taimur.d <taimur.saeed.malik@gmail.com>
2019-01-25 14:05:58 +0500

    a

---
+++ README.md
@@ -0,0 +1,11 @@
+# Assets Optimization
+
+Optimize your assets (PNG, JPG, SVG) with best optimization options
+
+## Usage
+
+Place your assets in ```src``` folder
+```bash
+gulp
+```
+Optimized assets will appear in ```dist``` folder
---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
+var imageminPngquant = require('imagemin-pngquant');
+var imageminZopfli = require('imagemin-zopfli');
+var imageminMozjpeg = require('imagemin-mozjpeg');
+var imageminGiflossy = require('imagemin-giflossy');
+var notifier = require('node-notifier');
+var runSequence = require('run-sequence');
+
+var paths = {
+  srcIMAGES: "src/*",
+  distIMAGES: "dist"
+}
+
+gulp.task('optimize', function () {
+  return gulp.src(paths.srcIMAGES)
+    .pipe(imagemin([
+      //png
+      imageminPngquant({
+        speed: 1,
+        quality: 80 //lossy settings
+      }),
+      imageminZopfli({
+        more: true
+      }),
+      //gif
+      // imagemin.gifsicle({
+      //     interlaced: true,
+      //     optimizationLevel: 3
+      // }),
+      //gif very light lossy, use only one of gifsicle or Giflossy
+      imageminGiflossy({
+        optimizationLevel: 3,
+        optimize: 3, //keep-empty: Preserve empty transparent frames
+        lossy: 2
+      }),
+      //svg
+      imagemin.svgo({
+        plugins: [{
+          removeViewBox: false
+        }]
+      }),
+      //jpg lossless
+      imagemin.jpegtran({
+        progressive: true
+      }),
+      //jpg very light lossy, use vs jpegtran
+      imageminMozjpeg({
+        quality: 80
+      })
+    ]))
+    .pipe(gulp.dest(paths.distIMAGES))
+});
+
+
+gulp.task('notify', function () {
+  notifier.notify({
+    title: 'Image Optimization',
+    message: 'Assests optimized successfully'
+  })
+});
+
+gulp.task('default', function (callback) {
+  runSequence('optimize', 'notify');
+});
